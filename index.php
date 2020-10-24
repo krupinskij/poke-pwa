add05a0625bd315ccb326b0ee476ff5c3174ab92
krupinskij <jk.krupinski@gmail.com>
2020-10-25 00:34:32 +0200

    Change comment in index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-// Ten plik jest mi potrzebny tylko do deploya na heroku
-// Hindusi z jutuba mi polecali
+# Ten plik jest mi potrzebny tylko do deploya na heroku
+# Hindusi z jutuba mi polecali
 <?php
 include_once("home.html");
 ?>
